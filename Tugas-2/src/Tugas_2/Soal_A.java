7dd51ff5964030781c67856ab585c2faaa5e2b1d
hp <hp@INTERN-AMALIA>
2023-02-22 19:32:34 +0700

    Tugas 2

---
+++ Tugas-2/src/Tugas_2/Soal_A.java
@@ -0,0 +1,18 @@
+package Tugas_2;
+
+public class Soal_A {
+	public static void main(String[] args) {
+		int n = 9;
+		
+		for (int i = 1; i < n + 1; i++) 
+		{
+			for (int j = 1; j < n + 1; j++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ Tugas-2/src/Tugas_2/Soal_D.java
@@ -0,0 +1,22 @@
+package Tugas_2;
+
+public class Soal_D {
+	public static void main(String[] args){
+        int n = 9;
+        int k = (n * 2) - 1;	
+		
+		for (int i = 1 ; i <= k; i += 2 ) 
+		{
+			for (int j = 1 ; j <= k; j += 2 ) 
+			{
+				if(j == i || j == k - i + 1) 
+				{
+					System.out.print(i);
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+    }
+}
