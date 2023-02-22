b7f3da11054f66318bfb2dc7f2301c90cac95a25
Amalia <110672446+amlya03@users.noreply.github.com>
2023-02-22 19:17:57 +0700

    Update Soal_A.java

--- Tugas_2/src/Soal_A.java
+++ Tugas_2/src/Soal_A.java
@@ -11,9 +11,9 @@ public class Soal_A {
 	public static void main(String[] args) {
 		int n = 9;
 		
-		for (int i = 0; i < n; i++) 
+		for (int i = 0; i < n + 1; i++) 
 		{
-			for (int j = 0; j < n; j++) 
+			for (int j = 0; j < n + 1; j++) 
 			{
 				System.out.print("*");
 			}
