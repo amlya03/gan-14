a064dd0fa4678370a9753f9307482fbf4c54741f
hp <hp@INTERN-AMALIA>
2023-02-28 18:01:27 +0700

    Tugas 3

--- Tugas_2/src/Soal_A.java
+++ Tugas_2/src/Soal_A.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-import java.util.Scanner;
+
 /**
  * @author Amalia
  *
@@ -11,13 +11,13 @@ public class Soal_A {
 	public static void main(String[] args) {
 		int n = 9;
 		
-		for (int i = 0; i < n; i++) 
+		for (int i = 1; i < n + 1; i++) 
 		{
-			for (int j = 0; j < n; j++) 
+			for (int j = 1; j < n + 1; j++) 
 			{
 				System.out.print("*");
 			}
-			System.out.println("*");
+			System.out.println();
 		}
 		
 	}
